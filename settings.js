4e66ecdbe632cbf28c3ba36c54efe7a7c9c22b1a
Karolina Kwiatkowska <48014678+karokwiat@users.noreply.github.com>
2021-12-06 11:30:55 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,7 +4,7 @@ export default function () {
     window.location.host.indexOf("localhost") != -1;
 
   const localApiUrl = "http://localhost:8080";
-  const prodApiUrl = "http://54.175.181.176:9090";
+  const prodApiUrl = "http://52.90.82.222:9090";
 
   // when fetching data from the api we need to know what the url is
   // It is different if you are developing locally or have the site deployed
