7f0597a280495a2f337893a2702d7e8017b2d244
Veselin Ivanov <v3s1ez@gmail.com>
2021-12-06 22:45:18 +0100

    refactor callback hell

--- pages/movie/movie.js
+++ pages/movie/movie.js
@@ -1,35 +1,28 @@
-export default (movieId) => {
+export default async (movieId) => {
   const content = document.querySelector(".content");
 
-  fetch("./pages/movie/movie.html")
-    .then((response) => response.text())
-    .then((movieHtml) => {
-      content.innerHTML = movieHtml;
+  const moviePageResponse = await fetch("./pages/movie/movie.html");
+  const movieHtml = await moviePageResponse.text();
+  content.innerHTML = movieHtml;
 
-      return fetch(`${window.apiUrl}/api/movie/${movieId}`)
-        .then((response) => response.json())
-        .then((movie) => {
-          console.log(movie);
-          document.querySelector(".movie-poster img").src = movie.poster;
-          document.querySelector("h3.title").innerText = movie.title;
-          document.querySelector("p.description").innerHTML = movie.description;
-          document.querySelector("p.ageRestriction").innerHTML =
-            movie.ageRestriction;
-          document.querySelector("p.rating").innerHTML = movie.rating;
-          document.querySelector("span").innerHTML = getTimeForCustomer(
-            movie.timeSlots[0].scheduledTime
-          );
-          const timeSlotsList = document.querySelector(".dropdown-content");
-          movie.timeSlots.forEach((timeSlot) => {
-            const timeSlotItem = document.createElement("a");
+  const getMovieResponse = await fetch(`${window.apiUrl}/api/movie/${movieId}`);
+  const movie = await response.json();
+  console.log(movie);
+  document.querySelector(".movie-poster img").src = movie.poster;
+  document.querySelector("h3.title").innerText = movie.title;
+  document.querySelector("p.description").innerHTML = movie.description;
+  document.querySelector("p.ageRestriction").innerHTML = movie.ageRestriction;
+  document.querySelector("p.rating").innerHTML = movie.rating;
+  document.querySelector("span").innerHTML = getTimeForCustomer(
+    movie.timeSlots[0].scheduledTime
+  );
+  const timeSlotsList = document.querySelector(".dropdown-content");
+  movie.timeSlots.forEach((timeSlot) => {
+    const timeSlotItem = document.createElement("a");
 
-            timeSlotItem.textContent = getTimeForCustomer(
-              timeSlot.scheduledTime
-            );
-            timeSlotsList.appendChild(timeSlotItem);
-          });
-        });
-    });
+    timeSlotItem.textContent = getTimeForCustomer(timeSlot.scheduledTime);
+    timeSlotsList.appendChild(timeSlotItem);
+  });
 
   function getTimeForCustomer(datetime) {
     const t = datetime.split(/[T]/);
