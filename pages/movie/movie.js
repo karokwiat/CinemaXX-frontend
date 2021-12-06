77ee105b7145465a8b31e47b9589b84dbc295c39
Veselin Ivanov <v3s1ez@gmail.com>
2021-12-06 22:50:19 +0100

    bla

--- pages/movie/movie.js
+++ pages/movie/movie.js
@@ -6,17 +6,20 @@ export default async (movieId) => {
   content.innerHTML = movieHtml;
 
   const getMovieResponse = await fetch(`${window.apiUrl}/api/movie/${movieId}`);
-  const movie = await response.json();
+  const movie = await getMovieResponse.json();
   console.log(movie);
   document.querySelector(".movie-poster img").src = movie.poster;
   document.querySelector("h3.title").innerText = movie.title;
   document.querySelector("p.description").innerHTML = movie.description;
   document.querySelector("p.ageRestriction").innerHTML = movie.ageRestriction;
   document.querySelector("p.rating").innerHTML = movie.rating;
-  document.querySelector("span").innerHTML = getTimeForCustomer(
-    movie.timeSlots[0].scheduledTime
-  );
+  // document.querySelector("span").innerHTML = getTimeForCustomer(
+  //   movie.timeSlots[0].scheduledTime
+  // );
+
   const timeSlotsList = document.querySelector(".dropdown-content");
+  timeSlotsList.addEventListener("change", handleTimeSlotChange);
+
   movie.timeSlots.forEach((timeSlot) => {
     const timeSlotItem = document.createElement("option");
 
@@ -30,4 +33,6 @@ export default async (movieId) => {
     const h = t[1].split(/[:]/);
     return `${t[0]} ${h[0]}:${h[1]}`;
   }
+
+  function handleTimeSlotChange(event) {}
 };
